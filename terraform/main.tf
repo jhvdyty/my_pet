bd45c9747c64dc4440f34711fab610455b3aecc6
Vlodimir <asakira884@gmail.com>
2025-08-05 00:42:50 +0300

    add terraform for docker (test) 4

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,21 +20,24 @@ resource "docker_image" "postgres" {
 resource "docker_container" "postgres" {
     name  = "postgres"
     image = docker_image.postgres.name
+    
     networks_advanced {
         name = docker_network.microservices.name
     }
+    
     env = [
         "POSTGRES_USER=postgres",
         "POSTGRES_PASSWORD=postgres",
         "POSTGRES_DB=mydb"
     ]
+    
     ports {
         internal = 5432
         external = 5432
     }
+    
     volumes {
         container_path = "/var/lib/postgresql/data"
-        source = "${path.module}/postgres_data"
+        host_path = "${path.module}/postgres_data"
     }
-  
 }

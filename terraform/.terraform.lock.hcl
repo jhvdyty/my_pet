ed3b13402489a2951e21d85e61d4d23dfca5f7ee
Vlodimir <asakira884@gmail.com>
2025-08-05 22:04:45 +0300

    add terraform yandex.cloud

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version     = "0.148.0"
+  constraints = "0.148.0"
+  hashes = [
+    "h1:KuKNTp3kFFV0LpqMNqZJAbxPntPu+AOpzSR+s/JFoBo=",
+    "zh:35152a2a92ffecea68d935c6d3d3c3ffdcc373f5d5356edc04a6eb5b9f1d4bf7",
+    "zh:38c72ef6d4a4eece84e262f19c26eddb787dfece2bdb307b63ce3a8990b9d33f",
+    "zh:5fb7b78356b98d15bb0437a493568ce28068ea796f80f56bbe561b39afb4ad3a",
+    "zh:6e42500954f8d9fd9fcf552bc0cae1159315dd820fd465d8c5deac2d0c716299",
+    "zh:818fe21ea21b1710da6557f26eaed35a8a38bddc2a24b16728d64217b2468f55",
+    "zh:927bc825fb5f19112e54af2f5760245873a0e6718d3be5e6f78e580afd0fb1fd",
+    "zh:9986a7f5fcc760421395e111930d05857e0e3aaf8cbb5d8a291f82d4404060d8",
+    "zh:b0df24aa09eb463d6da197df68f0bb1f6970afd2ac36b07f3f3b74a539a614cf",
+    "zh:baf0ce0eaa95357b92a8d9fa0c767ca14fe25644adecd34168d7bb8ec708c7de",
+    "zh:da40d391d36d6d5d011bcb7cef1382e6626ccfe0c0eebe794d187a94c6bc3b98",
+    "zh:e89e0aeb9749dd7b4bafae63f6f84190a7f06be33bc3b44f8ef07cea79ee002f",
+    "zh:eb8bb6f151641c3e9824f901a8b6eb8e96104f8068e5f83983042480a6df3aa0",
+    "zh:ff7353093e91298382af864957ea8c92271f8c82368115da4d81dc88617914ba",
+  ]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,97 @@
-terraform {
-    required_providers {
-        docker = {
-            source  = "kreuzwerker/docker"
-            version = "~> 3.0.2"
-        }
-    }
+
+locals {
+    backet_name = "tf-info-site-bucket"
+    index = "index.html"
 }
 
-provider "docker" {}
-
-resource "docker_network" "microservices" {
-    name = "microservices"
+// Create SA
+resource "yandex_iam_service_account" "sa" {
+  folder_id = local.folder_id
+  name      = "tf-test-sa"
 }
 
-resource "docker_image" "postgres" {
-    name = "postgres:16.0"
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = local.folder_id
+  role      = "storage.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
 }
 
-resource "docker_container" "postgres" {
-    name  = "postgres"
-    image = docker_image.postgres.name
-    
-    networks_advanced {
-        name = docker_network.microservices.name
-    }
-    
-    env = [
-        "POSTGRES_USER=postgres",
-        "POSTGRES_PASSWORD=postgres",
-        "POSTGRES_DB=mydb"
-    ]
-    
-    ports {
-        internal = 5432
-        external = 5432
-    }
-    
-    volumes {
-        container_path = "/var/lib/postgresql/data"
-        host_path = abspath("${path.module}/postgres_data")
-    }
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
 }
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = local.backet_name
+  acl    = "public-read"
+
+  website {
+    index_document = local.index
+  }
+}
+
+
+resource "yandex_storage_object" "index" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = yandex_storage_bucket.test.id
+  acl    = "public-read"
+  key    = local.index
+  source = "${local.index}"
+}
+
+
+output site_name {
+  value = yandex_storage_bucket.test.website_endpoint
+}
+
+
+
+//terraform {
+//    required_providers {
+//        docker = {
+//            source  = "kreuzwerker/docker"
+//            version = "~> 3.0.2"
+//        }
+//    }
+//}
+//
+//provider "docker" {}
+//
+//resource "docker_network" "microservices" {
+//    name = "microservices"
+//}
+//
+//resource "docker_image" "postgres" {
+//    name = "postgres:16.0"
+//}
+//
+//resource "docker_container" "postgres" {
+//    name  = "postgres"
+//    image = docker_image.postgres.name
+//    
+//    networks_advanced {
+//        name = docker_network.microservices.name
+//    }
+//    
+//    env = [
+//        "POSTGRES_USER=postgres",
+//        "POSTGRES_PASSWORD=postgres",
+//        "POSTGRES_DB=mydb"
+//    ]
+//    
+//    ports {
+//        internal = 5432
+//        external = 5432
+//    }
+//    
+//    volumes {
+//        container_path = "/var/lib/postgresql/data"
+//        host_path = abspath("${path.module}/postgres_data")
+//    }
+//}
